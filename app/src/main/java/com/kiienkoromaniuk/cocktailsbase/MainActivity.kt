7f1eb8e5383fae411415508637c873df58347134
FunnyCPP <kiienko.romaniuk@gmail.com>
2022-01-30 17:14:22 +0100

    Created MainActivity.kt

---
+++ app/src/main/java/com/kiienkoromaniuk/cocktailsbase/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kiienkoromaniuk.cocktailsbase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
