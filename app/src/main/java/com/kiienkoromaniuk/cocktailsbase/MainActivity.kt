529bb0f5d153874f679a74603bd3f47de23d16b2
FunnyCPP <kiienko.romaniuk@gmail.com>
2022-01-30 17:42:19 +0100

    Created app Implemented hilt

--- app/src/main/java/com/kiienkoromaniuk/cocktailsbase/MainActivity.kt
+++ app/src/main/java/com/kiienkoromaniuk/cocktailsbase/MainActivity.kt
@@ -2,10 +2,19 @@ package com.kiienkoromaniuk.cocktailsbase
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            val isDarkModeValue = true //isSystemInDarkTheme()
+
+            val isDarkMode = remember { mutableStateOf(isDarkModeValue) }
+        }
     }
 }
